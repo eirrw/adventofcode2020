948b220ceba577541e3959645b89b43e6a315222
Erik Wilson <tech+git@virunus.com>
2020-12-07 18:20:02 -0500

    day 5 part 2

--- 05/05.py
+++ 05/05.py
@@ -4,7 +4,7 @@ import statistics
 with open('input') as inputFile:
     data = inputFile.readlines()
 
-maxId = 0
+ids = {}
 for seat in data:
     rowStr = seat[0:7]
     colStr = seat[7:]
@@ -24,12 +24,18 @@ for seat in data:
         else:
             colVals[0] = math.ceil(statistics.median(colVals))
 
-    if rowVals[0] != rowVals[1] or colVals[0] != colVals[1]:
-        raise IndexError("Min and max does not match")
+    rowId = rowVals[0] * 8
+    if ids.get(rowId) is None:
+        ids[rowId] = [colVals[0]]
+    else:
+        ids[rowId].append(colVals[0])
 
-    id = rowVals[0] * 8 + colVals[0]
-    maxId = max(maxId, id)
+# p1
+print("Part 1: {}".format(max(ids.keys()) + max(ids[max(ids.keys())])))
 
-    print("row: {}, col: {}, id: {}".format(rowVals[0], colVals[0], id))
-
-print(maxId)
+# p2
+for k in ids.keys():
+    if k != min(ids.keys()) and k != max(ids.keys()) and len(ids[k]) != 8:
+        for l in range(0,8):
+            if l not in ids[k]:
+                print("Part 2: {}".format(k + l))
